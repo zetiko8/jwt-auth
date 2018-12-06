7f94a8eff5df91dd8286dcc673e56c8011d0383f
Anze Kolsek <anze.kolsek@gmail.com>
2018-12-06 20:41:32 +0100

    Test for signup completed

--- src/app.js
+++ src/app.js
@@ -1,22 +1,34 @@
 /**
+ * @module AuthenticateModule
+ */
+
+/**
  * Middleware system for jwt authentication
  * @class 
  */
-class authenticate {
+class Authenticate {
+
+    /**
+     * Callback to run after user has been saved, and to handle errors
+     * @callback saveUserCallback
+     * @param {Object[]|Error} err - Array of invalid user input errors, or an internal Error
+     * @param {string} err[].key - The key of singup data that was invalid
+     * @param {string} err[].reason - Reason why the key was invalid  
+     */
 
     /**
      * Function that retrieves a User from db based on given primary key
      * @typedef getUser
      * @type {Function}
-     * @param {string} key Primary key, by which the db will identify user
+     * @param {string} key - Primary key, by which the db will identify user
      */
 
     /**
      * Function that stores a User in db
      * @typedef saveUser
      * @type {Function}
-     * @param {Object} data User data to insert example: { username: 'username', password: 'password' }
-     * @param {saveUserCallback} cb 
+     * @param {Object} data - User data to insert example: { username: 'username', password: 'password' }
+     * @param {saveUserCallback} cb - Callback to run after user has been saved, and to handle errors
      */
 
     /**
@@ -25,12 +37,51 @@ class authenticate {
      * @param {saveUser} saveUser 
      */
     constructor(getUser, saveUser) {
-        this.getUser = getUser,
+        this.getUser = getUser
         this.saveUser = saveUser
+        this.test = 3
+    }
 
-        // Methods 
-        this.signup = require('./signup')
+    /**
+     * Middleware function for new user singup
+     * @typedef signupMiddleware
+     * @function
+     * @param {Object} req - Express request object
+     * @param {Object} res - Express response object
+     * @param {Function} next - Express next middleware function
+     */
+
+    // Methods
+    /**
+     * Method that returns middleware function for new user singup
+     * @name Authenticate#signup
+     * @method 
+     * @public
+     * @returns {signupMiddleware} 
+     */
+    signup() {
+        var self = this
+        return function (req, res, next) {
+            var user = {
+                username: req.body.username,
+                password: req.body.password
+            }
+            self.saveUser(user, function (err) {
+                if (err) {
+                    if (err === "user-policy") {
+                        res.sendStatus(400)
+                    }
+                    else next(500)
+                }
+                else next()
+            })
+        }
+    }
+
+    login() {
+        var self = this
+        
     }
 }
 
-module.exports = authenticate
+module.exports = Authenticate
